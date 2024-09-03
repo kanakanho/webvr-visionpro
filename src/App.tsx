79153899b8e992ca7e8fdceafd9e3d1897ecb395
kanakanho <kahoshibakichi@gmail.com>
2024-09-03 18:41:09 +0900

    init webvr

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { Canvas } from "@react-three/fiber";
+import { XR, createXRStore } from "@react-three/xr";
+import { useState } from "react";
+import "./App.css";
+
+const store = createXRStore();
+
+function App() {
+  const [red, setRed] = useState(true);
+  const [isVR, setIsVR] = useState(false);
+
+  return (
+    <>
+      {!isVR && (
+        <div className="buttonContainer">
+          <button
+            type="button"
+            onClick={() => {
+              store.enterVR();
+              setIsVR(true);
+            }}
+          >
+            Enter VR
+          </button>
+        </div>
+      )}
+      <Canvas>
+        <XR store={store}>
+          <mesh
+            pointerEventsType={{ deny: "grab" }}
+            onClick={() => setRed(!red)}
+            position={[0, 1, -1]}
+          >
+            <boxGeometry />
+            <meshBasicMaterial color={red ? "red" : "blue"} />
+          </mesh>
+        </XR>
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
